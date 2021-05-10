06fdc0930c90e5f03b2caee6fbf648a3344b568b
CosmicBee <76642947+CosmicBee@users.noreply.github.com>
2021-05-10 02:27:12 -0700

    2 button
    added weed button functions it now shows in div on click

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
 let a=document.querySelector("#shatter");
-let b= 6*9
+let b= document.getElementById("input");
+
+
 
 a.addEventListener("click", e => {
   console.log(e.target) 
 });
 
 function shatterr(){
-  document.getElementById("header1").innerHTML = "Shatter Amounts "
+  
+  document.getElementById("header1").innerHTML = "Shatter "
   document.getElementById("p1").innerHTML = `1 Gram: $${b}` 
-   document.getElementById("p2").innerHTML = "7g-Quarter: $ "
-   document.getElementById("p3").innerHTML = "14g-Half: $ "
-   document.getElementById("p4").innerHTML = "28g-Ounce: $ "
+  document.getElementById("p2").innerHTML = "3G: $ "
+   document.getElementById("p3").innerHTML = "7g-Quarter: $ "
+   document.getElementById("p4").innerHTML = "14g-Half: $ "
+   document.getElementById("p5").innerHTML = "28g-Ounce: $ "
+};
+
+
+
+
+let w=document.querySelector("#weed");
+
+w.addEventListener("click", e => {
+  console.log(e.target) 
+});
+
+function weedd(){
+  
+  document.getElementById("header1").innerHTML = "Weed"
+  document.getElementById("p1").innerHTML = `1 Gram: $` 
+  document.getElementById("p2").innerHTML = "Eighth: $ "
+   document.getElementById("p3").innerHTML = "7g-Quarter: $ "
+   document.getElementById("p4").innerHTML = "14g-Half: $ "
+   document.getElementById("p5").innerHTML = "28g-Ounce: $ "
 };
