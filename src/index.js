d84a4b8e0685d65089be958b60d2399c218170eb
CosmicBee <76642947+CosmicBee@users.noreply.github.com>
2021-05-09 23:47:27 -0700

    added
    More units to the dropdown and some js  touch ups.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 let a=document.querySelector("#shatter");
-
+let b= 6*9
 
 a.addEventListener("click", e => {
   console.log(e.target) 
@@ -7,8 +7,8 @@ a.addEventListener("click", e => {
 
 function shatterr(){
   document.getElementById("header1").innerHTML = "Shatter Amounts "
-  document.getElementById("p1").innerHTML = "1 Gram: "
-   document.getElementById("p2").innerHTML = "7g-Quarter: "
-   document.getElementById("p3").innerHTML = "14g-Half: "
-   document.getElementById("p4").innerHTML = "28g-Ounce: "
+  document.getElementById("p1").innerHTML = `1 Gram: $${b}` 
+   document.getElementById("p2").innerHTML = "7g-Quarter: $ "
+   document.getElementById("p3").innerHTML = "14g-Half: $ "
+   document.getElementById("p4").innerHTML = "28g-Ounce: $ "
 };
