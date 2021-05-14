8b93a05adf9501345eb4a0f199aa2377c1738c46
CosmicBee <76642947+CosmicBee@users.noreply.github.com>
2021-05-13 23:03:15 -0700

    weight and cost working
    weight and cost bars work now and are set to multiply with eachother for now.

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ a.addEventListener("click", e => {
 function shatterr(){
   let shatterElement=document.querySelector("#header1");
   let shatterCost=document.querySelector("#cost").value;
+  let weedWeight=document.querySelector("#weight").value;
   shatterElement.innerHTML = shatterCost;
 
   document.getElementById("header1").innerHTML = "Shatter "
-  document.getElementById("p1").innerHTML = `1 Gram: $${shatterCost}` 
+  document.getElementById("p1").innerHTML = `1 Gram: $${shatterCost*weedWeight}` 
   document.getElementById("p2").innerHTML = "3G: $ "
    document.getElementById("p3").innerHTML = "7g-Quarter: $ "
    document.getElementById("p4").innerHTML = "14g-Half: $ "
@@ -27,11 +28,12 @@ w.addEventListener("click", e => {
 function weedd(){
   let weedElement =document.querySelector("#header1");
   let weedCostH = document.querySelector("#cost").value;
+  let weedWeight=document.querySelector("#weight").value;
   weedElement.innerHTML = weedCostH;
 
   document.getElementById("header1").innerHTML = "Weed"
-  document.getElementById("p1").innerHTML = `1 Gram: $ ${weedCostH}` 
-  document.getElementById("p2").innerHTML = "Eighth: $ "
+  document.getElementById("p1").innerHTML = `1 Gram: $ ${weedCostH * weedWeight}` 
+  document.getElementById("p2").innerHTML = "Eighth: $  "
    document.getElementById("p3").innerHTML = "7g-Quarter: $ "
    document.getElementById("p4").innerHTML = "14g-Half: $ "
    document.getElementById("p5").innerHTML = "28g-Ounce: $ "
