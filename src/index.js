83f76ace8fba7195c16fdb0940692776713f1438
CosmicBee <76642947+CosmicBee@users.noreply.github.com>
2021-05-13 17:22:23 -0700

    js show cost
    Made the inputted value for cost put on div display.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-let a=document.querySelector("#shatter");
 
-let first = 10;
-let second=4;
-let b= first*second;
+
+
 
 
 a.addEventListener("click", e => {
@@ -10,9 +8,12 @@ a.addEventListener("click", e => {
 });
 
 function shatterr(){
-  
+  let shatterElement=document.querySelector("#header1");
+  let shatterCost=document.querySelector("#cost").value;
+  shatterElement.innerHTML = shatterCost;
+
   document.getElementById("header1").innerHTML = "Shatter "
-  document.getElementById("p1").innerHTML = `1 Gram: $${b}` 
+  document.getElementById("p1").innerHTML = `1 Gram: $${shatterCost}` 
   document.getElementById("p2").innerHTML = "3G: $ "
    document.getElementById("p3").innerHTML = "7g-Quarter: $ "
    document.getElementById("p4").innerHTML = "14g-Half: $ "
@@ -29,9 +30,13 @@ w.addEventListener("click", e => {
 });
 
 function weedd(){
-  
+  let weedElement =document.querySelector("#header1");
+
+  let weedCostH = document.querySelector("#cost").value;
+  weedElement.innerHTML = weedCostH;
+
   document.getElementById("header1").innerHTML = "Weed"
-  document.getElementById("p1").innerHTML = `1 Gram: $` 
+  document.getElementById("p1").innerHTML = `1 Gram: $ ${weedCostH}` 
   document.getElementById("p2").innerHTML = "Eighth: $ "
    document.getElementById("p3").innerHTML = "7g-Quarter: $ "
    document.getElementById("p4").innerHTML = "14g-Half: $ "
